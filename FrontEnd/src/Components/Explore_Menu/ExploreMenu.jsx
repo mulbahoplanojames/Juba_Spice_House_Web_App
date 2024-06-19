27565298c58d305286b35a4d8e641571403c25d6
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-06-19 02:00:57 +0200

    95% done with the home hero section and 97% done with the navbar

--- FrontEnd/src/Components/Explore_Menu/ExploreMenu.jsx
+++ FrontEnd/src/Components/Explore_Menu/ExploreMenu.jsx
@@ -1,5 +1,29 @@
+import { menu_list } from "../../data/assets";
+
 const ExploreMenu = () => {
-	return <div>ExploreMenu</div>;
+	return (
+		<>
+			<div className=''>
+				<h1 className='text-4xl font-bold '>Explore Our Menu</h1>
+				<p className=''>
+					Choose from a diverse selection featuring a delectable array of dishes
+					crafted with finest ingredients from around the world. Our mission is
+					to satisfy your dinning experience, one delicious meal at a time.
+				</p>
+				<div className=''>
+					{
+						/* Mapping through the menu items */
+						menu_list.map((item, index) => (
+							<div key={index} className=''>
+								<img src={item.menu_image} alt='menu images' className='' />
+								<p>{item.menu_name}</p>
+							</div>
+						))
+					}
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default ExploreMenu;
